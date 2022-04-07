8c4889dd322b6810f19350ef8877a7323d8eed97
RafaelaGomes <rafahh97@gmail.com>
2022-04-07 09:23:42 -0300

    add mainpage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
 import { LoginComponent } from './login/login.component';
+import { MainpageComponent } from './mainpage/mainpage.component';
 import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [
@@ -21,6 +22,10 @@ const routes: Routes = [
   { 
     path: 'signup', 
     component: SignupComponent 
+  },
+  { 
+    path: 'mainpage', 
+    component: MainpageComponent 
   },
 ];
 
